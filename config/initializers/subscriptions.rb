21276110e83601147eed0de08d04e6ca1687bcb0
David Doan <david@avvo.com>
2014-01-24 22:13:12 -0800

    remove debug output

--- config/initializers/subscriptions.rb
+++ config/initializers/subscriptions.rb
@@ -2,5 +2,3 @@ mailchimp_api_key = ENV['MAILCHIMP_API_KEY']
 
 Subscription.mailchimp_client = Mailchimp::API.new(mailchimp_api_key) if mailchimp_api_key
 Subscription.mailchimp_list_id = ENV['MAILCHIMP_LIST_ID']
-
-puts Subscription.mailchimp_client
