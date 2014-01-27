5ccb53af480593550827971047d2b8604e7a98d3
David Doan <david@avvo.com>
2014-01-26 18:33:24 -0800

    pull rails secret key from environment

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Hive::Application.config.secret_key_base = '7ce01ce12971f8ba16778d97c23415666100a238c1741707dd4918392a5c37794c5194896f5fbbf6f17f1fba7ddf4cdbdc7480e7ca88dbca8b025300b8b0e8c5'
+
+# Secret key used for development and testing
+DEFAULT_SECRET_KEY = '7ce01ce12971f8ba16778d97c23415666100a238c1741707dd4918392a5c37794c5194896f5fbbf6f17f1fba7ddf4cdbdc7480e7ca88dbca8b025300b8b0e8c5'
+
+Hive::Application.config.secret_key_base = ENV['RAILS_SECRET_KEY'] || DEFAULT_SECRET_KEY
