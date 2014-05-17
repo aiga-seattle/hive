1c7f86e7fec55c57d1174feade6f2cdf1d2edb07
Steven Chau <steven.p.chau@gmail.com>
2014-05-17 10:54:08 -0700

    Tag registration URL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def registration_url
-    "http://hive-seattle-2014.eventbrite.com"
+    "http://hive-seattle-2014.eventbrite.com?ref=ebtnebregn"
   end
 
   def headshot_image_tag(filename, options = {width: "100%"})
