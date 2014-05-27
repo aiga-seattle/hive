81527642e3d429708af447c44dab0f3537bcad08
Steven Chau <steven.p.chau@gmail.com>
2014-05-27 11:43:28 -0700

    Reformatting Schedule to include room breakdowns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,18 @@ module ApplicationHelper
   def headshot_image_tag(filename, options = {width: "100%"})
     image_tag "/speakers/headshots/#{filename}", options
   end
+
+  def format_period(start_time, end_time)
+    "#{start_time}&nbsp;-&nbsp;#{end_time}".html_safe
+  end
+
+  def schedule_row(locals = {})
+    locals[:css_class] = "" unless locals[:css_class]
+    render partial: "schedule_row", locals: locals
+  end
+
+  def schedule_break(start_time, end_time)
+    render partial: "schedule_row_break", locals: {start_time: start_time, end_time: end_time}
+  end
+
 end
